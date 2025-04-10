f3823191b377198997e21de8c8b024b8f062b4be
DragonYu <guo1173245189@163.com>
2025-04-10 17:54:48 +0800

    苍穹外卖项目-员工账号启用禁用

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -12,9 +12,9 @@ import java.sql.SQLIntegrityConstraintViolationException;
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
-@RestControllerAdvice
-@Slf4j
-public class GlobalExceptionHandler {
+    @RestControllerAdvice
+    @Slf4j
+    public class GlobalExceptionHandler {
 
     /**
      * 捕获业务异常
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -6,6 +6,7 @@ import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface EmployeeMapper {
@@ -35,4 +36,5 @@ public interface EmployeeMapper {
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
+    void update(Employee employee);
 }
